f8ec5adf32ec73ed38b43cac90fe23b0158af02d
ArenaMirror03 <officialsumitsharma03@gmail.com>
2025-01-12 21:55:18 +0530

    Student Management System

---
+++ final(Phase1).py
@@ -0,0 +1,37 @@
+import json
+students = []
+counter=0
+while True:
+    print(" Menu:")
+    print("1. Enter student data")
+    print("2. Display all student records")
+    print("3. Exit")
+    choice = input("Enter your choice (1/2/3): ")
+    if choice == "1":
+        num_students = int(input("Enter the number of students: "))
+        for i in range(num_students):
+            print("\nEnter details for student:")
+            student_id = int(input("Enter student ID: "))
+            name = input("Enter student name: ")
+            age = int(input("Enter student age: "))
+            grade = input("Enter student grade: ")
+            
+            students.append({
+                'Student ID': student_id,
+                'Name': name,
+                'Age': age,
+                'Grade': grade
+            })
+            counter=1
+    elif choice == "2":
+        if counter ==0:
+            print("The list is empty ")
+        else:
+            jsondata=json.dumps(students,indent=4)
+            print(jsondata)
+        
+    elif choice == "3":
+        print("\nExiting program. Goodbye!")
+        break
+    else:
+        print("\nInvalid choice. Please enter 1, 2, or 3.")
