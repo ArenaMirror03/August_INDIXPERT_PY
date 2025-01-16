69a5cb1aaaa7d4192360a78dfcdeb12cac07a0f8
mohdmohsinrazakhan <mohdmohsinraza8755@gmail.com>
2025-01-16 17:06:06 +0530

    searchby_mobile_number

--- python__phase1/firstprogramof.py
+++ python__phase1/firstprogramof.py
@@ -1,15 +1,20 @@
 import json
+import os
+
 students = []
+qualifications = []
 counter=0
+
 while True:
+    print("==========================================================")
     print(" Menu:")
     print("1. Enter student data")
     print("2. Display all student records")
-    print("3. Exit")
+    print("3. For searching the data using mobile number")
+    print("4. Exit")
+    choice = input("Enter your choice (1/2/3/4): ")
 
-    choice = input("Enter your choice (1/2/3): ")
-
-    if choice == "1":
+    if choice == "1": 
         num_students = int(input("Enter the number of students: "))
 
         for i in range(num_students):
@@ -17,29 +22,64 @@ while True:
             student_id = int(input("Enter student ID: "))
             name = input("Enter student name: ")
             age = int(input("Enter student age: "))
-            grade = input("Enter student grade: ")
+            grade = input("Enter student grade: ") 
+            phone_number = int(input("Enter your mobile number: "))
 
-            
+            while True:
+                userinput=int(input("Do you want to add qualification(1: Yes || 0: No): "))
+                if userinput==1:
+                    education = {}
+                    education["Qualification name"] = input("Enter your qualication: ")
+                    education["Passing year"] = int(input("Enter your passing year: "))
+                    qualifications.append(education)
+                else:
+                    break    
+
             students.append({
                 'Student ID': student_id,
                 'Name': name,
                 'Age': age,
-                'Grade': grade
+                'Grade': grade,
+                'phone number':phone_number,
+                'qualification': qualifications
             })
             counter=1
 
     elif choice == "2":
-        if counter ==0:
-            print("The list is empty ")
+        if counter == 0:
+            print("The list is empty.")
 
         else:
+            path = os.getcwd()+"\\jsondata.json"
+            with open(path,"w") as f:
+                f.write(json.dumps(students,indent=4))
+            print("The json file is created")
 
-            jsondata=json.dumps(students,indent=4)
-            print(jsondata)
-        
     elif choice == "3":
+        if counter == 0:
+            print("No data is available.")
+   
+        else:
+            path = os.getcwd() + "\\jsondata.json"
+            with open(path, "r") as f:
+                data = json.load(f)
+
+            
+            userchoice=int(input("Enter the mobile number you want to search: "))
+            find = False
+            for student in data:
+                if student["phone number"] == userchoice:
+                    print("Student found: ", student) 
+                    find = True                    
+                    break
+
+            if not find:
+                print("No student found with this number.")
+                                     
+            
+    elif choice == "4":
         print("\nExiting program. Goodbye!")
         break
 
     else:
-        print("\nInvalid choice. Please enter 1, 2, or 3.")
+        print("\nInvalid choice. Please enter 1, 2, 3 or 4.")
